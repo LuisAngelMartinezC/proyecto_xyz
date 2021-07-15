5d9a25b17b8bea0d93de1650dbf12016e4974e05
LegendSlayer256 <serandkast@gmail.com>
2021-07-14 21:17:00 -0500

    database

--- registro.php
+++ registro.php
@@ -12,7 +12,7 @@
 <body>
 
     <div class="container">
-        <form method="POST" action="includes/form_registro_user.php">
+        <form method="POST" action="includes/procesos/form_registro_user.php">
             <div class="form-group">
                 <label for="identificación_user">Identificacion</label>
                 <input type="text" class="form-control" id="identificacion_user" name="identificacion_user">
@@ -42,7 +42,7 @@
             <button type="submit" class="btn btn-primary">Registrarse</button>
         </form>
     </div>
-    
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
